1d4fa9c7a7791775795da3c2bd604d04e32c38f0
worhol <trashuries@gmail.com>
2021-03-04 18:41:37 -0600

    Completed exercise 8

--- TMCProjects/mooc-java-programming-i/part01-Part01_08.Greeting/src/main/java/Greeting.java
+++ TMCProjects/mooc-java-programming-i/part01-Part01_08.Greeting/src/main/java/Greeting.java
@@ -5,8 +5,11 @@ public class Greeting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("What's your name?");
+        String name = scanner.nextLine();
+        System.out.println("Hi "+name);
 
-        // Write your program here
+
 
     }
 }
