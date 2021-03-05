5936eda325bda523ed4ffe4a4d820b73d72f2a1b
worhol <trashuries@gmail.com>
2021-03-04 18:58:53 -0600

    Completed exercise 9

--- TMCProjects/mooc-java-programming-i/part01-Part01_09.Conversation/src/main/java/Conversation.java
+++ TMCProjects/mooc-java-programming-i/part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -5,8 +5,12 @@ public class Conversation {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Greetings! How are you doing?");
+        String first = scanner.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        String second = scanner.nextLine();
+        System.out.println("Thanks for sharing!");
 
-        // Write your program here
 
     }
 }
